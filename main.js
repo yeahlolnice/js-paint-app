8cae455567e1b10d74a66390b223adabf86c5254
Matthew Williams <mattwilly98@hotmail.com>
2020-12-06 19:38:49 +1100

    RGB slider ready for color changing

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let redSlider = document.getElementById("redSlider");
+let greenSlider = document.getElementById("greenSlider");
+let blueSlider = document.getElementById("blueSlider");
+let redVal = document.getElementById("redVal");
+let greenVal = document.getElementById("greenVal");
+let blueVal = document.getElementById("blueVal");
+
 let paint = document.querySelector('#paint');
 let ctx = paint.getContext("2d");
 let x = 0;
@@ -7,12 +14,12 @@ let style = "#212121";
 let isDrawing = false;
 
 
-
 paint.addEventListener('mousedown', e => {
     x = e.clientX - rect.left;
     y = e.clientY - rect.top;  // gets mouse pos relative to the canvase
     isDrawing = true;
 })
+
 
 paint.addEventListener('mousemove', e => {
     if (isDrawing === true) {
@@ -21,6 +28,29 @@ paint.addEventListener('mousemove', e => {
         y = e.clientY - rect.top; // tracking where the mouse is going when holding down the mouse click
     }
 })
+
+
+// sliders value on sceen
+//update slider value
+redVal.innerHTML = redSlider.value;
+redSlider.oninput = function() {
+  redVal.innerHTML = this.value;
+}
+greenVal.innerHTML = greenSlider.value;
+greenSlider.oninput = function() {
+    greenVal.innerHTML = this.value;
+}
+blueVal.innerHTML = blueSlider.value;
+blueSlider.oninput = function() {
+    blueVal.innerHTML = this.value;
+}
+
+
+
+function Clear() {
+    ctx.clearRect(0, 0, paint.width, paint.height);
+}
+ 
 
 paint.addEventListener('mouseup', e => {
     x = e.clientX - rect.left;
@@ -43,3 +73,4 @@ function lineDraw(ctx, x1, y1, x2, y2, styleColor) {
     ctx.stroke();
     ctx.closePath();
 }
+
